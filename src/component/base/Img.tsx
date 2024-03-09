5cdc9b218cb383500c49823e1ad5323a020d6ba5
mym0404 <mym0404@gmail.com>
2024-03-09 23:27:04 +0900

    Release 0.0.28

--- src/component/base/Img.tsx
+++ src/component/base/Img.tsx
@@ -1,7 +1,10 @@
+import type { Ref } from 'react';
 import { forwardRef } from 'react';
 import type { ImageProps } from '@chakra-ui/next-js';
 import { Image } from '@chakra-ui/next-js';
 
-const Img = forwardRef((props: ImageProps, ref) => <Image ref={ref} {...props} />);
+const Img = forwardRef((props: ImageProps, ref: Ref<HTMLImageElement>) => (
+  <Image ref={ref} {...props} />
+));
 export { Img };
 export type { ImageProps as ImgProps };
--- src/component/base/Link.tsx
+++ src/component/base/Link.tsx
@@ -1,6 +1,10 @@
+import type { Ref } from 'react';
 import { forwardRef } from 'react';
 import type { LinkProps } from '@chakra-ui/next-js';
+import { Link as Inner } from '@chakra-ui/next-js';
 
-const Link = forwardRef((props: LinkProps, ref) => <Link ref={ref} {...props} />);
+const Link = forwardRef((props: LinkProps, ref: Ref<HTMLAnchorElement>) => (
+  <Inner ref={ref} {...props} />
+));
 export { Link };
 export type { LinkProps };
