dda35edf4f14bcf7394b438dbd39d7bde766840c
mym0404 <mym0404@gmail.com>
2024-03-09 23:24:23 +0900

    Release 0.0.27

--- src/component/base/Img.tsx
+++ src/component/base/Img.tsx
@@ -1,3 +1,7 @@
+import { forwardRef } from 'react';
+import type { ImageProps } from '@chakra-ui/next-js';
 import { Image } from '@chakra-ui/next-js';
 
-export { Image };
+const Img = forwardRef((props: ImageProps, ref) => <Image ref={ref} {...props} />);
+export { Img };
+export type { ImageProps as ImgProps };
--- src/component/base/Link.tsx
+++ src/component/base/Link.tsx
@@ -1,5 +1,6 @@
+import { forwardRef } from 'react';
 import type { LinkProps } from '@chakra-ui/next-js';
-import { Link } from '@chakra-ui/next-js';
 
+const Link = forwardRef((props: LinkProps, ref) => <Link ref={ref} {...props} />);
 export { Link };
 export type { LinkProps };
