1bc739dc0f7b8e8587b42a8a1901b977cd7338b4
mym0404 <mym0404@gmail.com>
2024-03-11 10:18:24 +0900

    Release 0.0.49

--- src/provider/DesignProvider.tsx
+++ src/provider/DesignProvider.tsx
@@ -1,6 +1,11 @@
 import type { PropsWithChildren } from 'react';
 import React from 'react';
-import { baseTheme, ChakraProvider, cookieStorageManagerSSR } from '@chakra-ui/react';
+import {
+  baseTheme,
+  ChakraProvider,
+  ColorModeScript,
+  cookieStorageManagerSSR,
+} from '@chakra-ui/react';
 import type { Dict } from '@chakra-ui/utils';
 import type { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 
@@ -13,11 +18,31 @@ type Props = PropsWithChildren<{
   cookies: RequestCookie[];
   cookiesString: string;
   theme?: Dict;
+  initialColorMode?: 'dark' | 'ligh';
+  enableColorModeScript?: boolean;
 }>;
 
-export const DesignProvider = ({ children, cookies, cookiesString, theme }: Props) => {
+export const DesignProvider = ({
+  children,
+  cookies,
+  cookiesString,
+  theme,
+  initialColorMode,
+  enableColorModeScript = true,
+}: Props) => {
   return (
     <InitialCookiesProvider cookies={cookies}>
+      {enableColorModeScript ? (
+        <ColorModeScript
+          initialColorMode={
+            initialColorMode ??
+            cookies.find((i) => i.name === 'chakra-ui-color-mode')?.value === 'dark'
+              ? 'dark'
+              : 'light'
+          }
+          type={'cookie'}
+        />
+      ) : null}
       <StyledComponentsRegistry>
         <ChakraProvider
           colorModeManager={cookieStorageManagerSSR(cookiesString)}
