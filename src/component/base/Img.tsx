dba93db915198a7918232380783ecc1304295c32
mym0404 <mym0404@gmail.com>
2024-03-17 23:41:16 +0900

    fix: change img prop type

--- src/component/base/Img.tsx
+++ src/component/base/Img.tsx
@@ -2,17 +2,25 @@ import type { Ref } from 'react';
 import React, { forwardRef } from 'react';
 import type { ImageProps } from '@chakra-ui/next-js';
 import { Image } from '@chakra-ui/next-js';
+import type CSS from 'csstype';
 
-type ImgProps = Omit<ImageProps, 'w' | 'width' | 'h' | 'height'> & {
+type ImgProps = Omit<ImageProps, 'w' | 'width' | 'h' | 'height' | 'objectFit'> & {
   w?: number | `${number}` | undefined;
   h?: number | `${number}` | undefined;
   width?: number | `${number}` | undefined;
   height?: number | `${number}` | undefined;
+  objectFit?: CSS.Property.ObjectFit;
 };
 
 const Img = forwardRef<HTMLImageElement, ImgProps>(
-  ({ w, h, width, height, ...rest }: ImgProps, ref: Ref<HTMLImageElement>) => (
-    <Image ref={ref} {...rest} width={width ?? w} height={height ?? h} />
+  ({ w, h, width, height, objectFit, style, ...rest }: ImgProps, ref: Ref<HTMLImageElement>) => (
+    <Image
+      ref={ref}
+      {...rest}
+      style={{ objectFit, ...style }}
+      width={width ?? w}
+      height={height ?? h}
+    />
   ),
 );
 export { Img };
