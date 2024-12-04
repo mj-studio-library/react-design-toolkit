8628b3e4d4a729bb36baad4995614773d26840b0
Mystic <mym0404@gmail.com>
2024-12-04 16:00:46 +0900

    feat: convert box to indirect exporter

--- src/component/base/View.tsx
+++ src/component/base/View.tsx
@@ -1,4 +1,9 @@
-import { Box as View, type BoxProps as ViewProps } from '@chakra-ui/react';
+import React, { forwardRef } from 'react';
+import { Box, type BoxProps as ViewProps, type StackProps } from '@chakra-ui/react';
+
+const View = forwardRef(({ as, ...props }: StackProps, ref) => (
+  <Box as={as} ref={ref} {...props} />
+));
 
 export { View };
 export type { ViewProps };
